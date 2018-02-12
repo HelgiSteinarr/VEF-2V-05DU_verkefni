f1ee18efa8da47ce1f270c891aba2c83947ca66e
Helgi Steinarr <HelgiSteinarr@users.noreply.github.com>
2018-02-12 02:41:21 +0000

    Add files via upload

---
+++ verk2/app/BookCase/bookcase.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App;
+
+class BookCase {
+    static $book_array = [
+        001 => ["name" => "Test bók 1", 
+                "desc" => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", 
+                "writer" => "Helgi",
+                "publisher" => "Fisedush ehf",
+                "year_published"=> "1950"],
+        002 => ["name" => "Test bók 2", 
+                "desc" => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", 
+                "writer" => "Guðmundur",
+                "publisher" => "Fisedush ehf",
+                "year_published"=> "1990"],
+        003 => ["name" => "Test bók 3", 
+                "desc" => "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.", 
+                "writer" => "Albert",
+                "publisher" => "Fisedush ehf",
+                "year_published"=> "2001"]];
+
+    function getBookInfo($bookId)
+    {
+        if (array_key_exists($bookId, $book_array))
+        {
+            return $book_array[$bookId];
+        }else{
+            return ["error" => "No book with the ID '" . strval($bookId) . "'"];
+        }
+    }
+
+    function getFullArray()
+    {
+        return $book_array;
+    }
+}
--- verk2/app/Http/Controllers/bookcase/bookCaseController.php
+++ verk2/app/Http/Controllers/bookcase/bookCaseController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\BookCase;
+
 class bookCaseController extends Controller{
 
     function index(){
-        return view("homepage");
+        $book_data = BookCase::getFullArray();
+        return view("homepage", ["book_data" => $book_data]);
     }
 
     function infopage($bookid){
-        return view("bookinfo");
+        $book_info = BookCase::getBookInfo($bookid);
+        return view("bookinfo", ["book_info" => $book_info]);
     }
 }
